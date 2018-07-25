a90a98a898555f9217beb7802ca36f82944f68c9
mujahed85 <mujahed.mca@gmail.com>
2018-07-25 07:50:27 +0530

    Bubble Sort File Added

---
+++ BubbleSort.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BubbleSort 
+{
+    public static void main(String[] args) 
+    { 
+        System.out.print("Ascending Order:");
+        for (int i = 0; i < 10; i++) 
+        {
+            System.out.println("i="+i);
+        }
+    }
+}
